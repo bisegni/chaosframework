8e15679639c8fea3af9e087f80f11aebe96cd8a0
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2012-01-04 14:14:47 +0100

    Correct the scons for deleting files

--- chaos/common/ChaosCommon.h
+++ chaos/common/ChaosCommon.h
@@ -13,28 +13,43 @@
 
 namespace chaos {
     
-    /**
-     Chaos base libreary for other toolkit sublcass
+   
+    //! Chaos common engine class
+    /*! 
+        This is the base class for the other toolkit, it thake care to initialize all common
+        resource used for the base chaos function
      */
     class ChaosCommon {
-        
-    protected:
-        ChaosCommon();
-        ~ChaosCommon();
-
-        /**
-         !CHAOS substrate initialization
-         */
-        virtual void init(int argc = 1, const char* argv[] = NULL) throw(CException);
-        /**
-         Try to search an ip for the local network interface
+        //! Network address scan
+        /*! 
+         This methdo will take care for network address scan process
          */
         void scanForLocalNetworkAddress();
-
-    public:
         
-        /**
-         return the global configuration
+    protected:
+        //! Constructor
+        /*! 
+          A more elaborate description of the destructor.
+         */
+        ChaosCommon();
+        
+        //! Destructor
+        /*! 
+         A more elaborate description of the destructor.
+         */
+        ~ChaosCommon();
+
+        //! Init method
+        /*! 
+         This virtual method can be extended by toolkit subclass for specialized initializaion
+         in themain toolkit subclass of ChaosCommon
+         */
+        virtual void init(int argc = 1, const char* argv[] = NULL) throw(CException);
+        
+    public:
+        //! Return the global configuration for the current process
+        /*! 
+         Return the GlobalConfiguration singleton instance
          */
         GlobalConfiguration *getGlobalConfigurationInstance();
     }; 
