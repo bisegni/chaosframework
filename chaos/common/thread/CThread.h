b332297925baf08c8f5a77ff38329deba64f055b
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2012-02-17 15:09:19 +0100

    add the licence to all file
    Change-Id: Ic6b34d2a6a6b48ea1ee1cdb1b613cc3e3036de30

--- chaos/common/thread/CThread.h
+++ chaos/common/thread/CThread.h
@@ -1,10 +1,23 @@
-    //
-    //  CThread.h
-    //  ChaosFramework
-    //
-    //  Created by Claudio Bisegni on 20/03/11.
-    //  Copyright 2011 INFN. All rights reserved.
-    //
+/*	
+ *	CThread.h
+ *	!CHOAS
+ *	Created by Bisegni Claudio.
+ *	
+ *    	Copyright 2012 INFN, National Institute of Nuclear Physics
+ *
+ *    	Licensed under the Apache License, Version 2.0 (the "License");
+ *    	you may not use this file except in compliance with the License.
+ *    	You may obtain a copy of the License at
+ *
+ *    	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    	Unless required by applicable law or agreed to in writing, software
+ *    	distributed under the License is distributed on an "AS IS" BASIS,
+ *    	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    	See the License for the specific language governing permissions and
+ *    	limitations under the License.
+ */
+
 #ifndef ControlSystemThread_H
 #define ControlSystemThread_H
 
--- chaos/cu_toolkit/ChaosCUToolkit.h
+++ chaos/cu_toolkit/ChaosCUToolkit.h
@@ -1,13 +1,23 @@
-/*
- *  ChaosCUToolkit.h
- *  ChaosFramework
+/*	
+ *	ChaosCUToolkit.h
+ *	!CHOAS
+ *	Created by Bisegni Claudio.
+ *	
+ *    	Copyright 2012 INFN, National Institute of Nuclear Physics
  *
- *  Created by Claudio Bisegni on 13/02/11.
- *  Copyright 2011 INFN. All rights reserved.
+ *    	Licensed under the Apache License, Version 2.0 (the "License");
+ *    	you may not use this file except in compliance with the License.
+ *    	You may obtain a copy of the License at
  *
+ *    	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    	Unless required by applicable law or agreed to in writing, software
+ *    	distributed under the License is distributed on an "AS IS" BASIS,
+ *    	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    	See the License for the specific language governing permissions and
+ *    	limitations under the License.
  */
 
-/* The classes below are not exported */
 #ifndef ChaosCUToolkit_H
 #define ChaosCUToolkit_H
     //#pragma GCC visibility push(hidden)
