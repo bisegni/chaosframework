b30c7b64206af7eec45688e9bef85518a0c64157
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2013-03-25 22:59:03 +0100

    clean of code
    Change-Id: I02d089fbe31a732f3e9f2f17deb0b0753b8e6f2f

--- ReadDataService.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ReadDataService.cpp
-//  CHAOSFramework
-//
-//  Created by Claudio Bisegni on 3/23/13.
-//  Copyright (c) 2013 INFN. All rights reserved.
-//
-
-#include "ReadDataService.h"
