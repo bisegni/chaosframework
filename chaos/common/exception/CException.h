1e29caddab6e187c7d32d2fd0b1d00d881ce62f8
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2013-05-05 21:56:13 +0200

    correct the bug on CException for the memory leak message
    Change-Id: Ie8442e4b0c1b7c35ceb464fcc7f07c5e5e1302d2

--- chaos/common/exception/CException.h
+++ chaos/common/exception/CException.h
@@ -24,38 +24,48 @@
 #include <exception>
 #include <sstream>
 
+#define MSG_ERROR_BUF 255
+
 using namespace std;
 namespace chaos{
     /*
      Base class for exception in control system library
      */
     class CException : public std::exception {
+        //! string stream for compose the "what" message
+        std::string msg;
+        
+        void composeMsg() {
+            std::stringstream ss;
+            ss << "-----------Exception------------";
+            ss << "Domain:" << errorDomain;
+            ss << "Message:" << errorMessage;
+            ss << "Error Code;" << errorCode;
+            ss << "-----------Exception------------";
+            const string& strSS = ss.str();
+            msg = strSS.c_str();
+        }
     public:
-        //identify the number for the error
+        //! identify the number for the error
         int errorCode;
-        //describe the error that occour
+        //! describe the error that occour
         string errorMessage;
-        //identify the domain(ControlUnit, DataManager, ....)
+        //! identify the domain(ControlUnit, DataManager, ....)
         string errorDomain;
+
         
         explicit CException(int eCode, const char * eMessage,  const char * eDomain):errorCode(eCode),
         errorMessage( eMessage, strlen( eMessage )),
-        errorDomain( eDomain, strlen( eDomain )){};
+        errorDomain( eDomain, strlen( eDomain )) {composeMsg();};
         
         explicit CException(int eCode, std::string& eMessage,  std::string& eDomain):errorCode(eCode),
         errorMessage(eMessage),
-        errorDomain(eDomain) {};
+        errorDomain(eDomain) {composeMsg();};
         
         virtual ~CException() throw() {};
         
         virtual const char* what() const throw() {
-            std::stringstream ss;
-            ss << "-----------Exception------------";\
-            ss << "Domain:" << errorDomain;\
-            ss << "Message:" << errorMessage;\
-            ss << "Error Code;" << errorCode;\
-            ss << "-----------Exception------------";
-            return ss.str().c_str();
+            return msg.c_str();
         }
     };
 }
