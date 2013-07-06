b770949406a1abe42c4fcd78665e973e878e36ed
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2013-07-06 19:36:16 +0200

    correct a little bug on macro definition
    Change-Id: I10b4bb56e685681166b3ec19437e23b63f2a0853

--- chaos/common/extension/impl/decl.hpp
+++ chaos/common/extension/impl/decl.hpp
@@ -15,15 +15,12 @@
 #ifndef BOOST_EXTENSION_DECL_HPP
 #define BOOST_EXTENSION_DECL_HPP
 
-#if __GNUC__ >= 4
+#if #if defined(__linux__) || defined(__APPLE__)
 #  define BOOST_EXTENSION_EXPORT_DECL __attribute__((visibility("default")))
 #  define BOOST_EXTENSION_IMPORT_DECL __attribute__((visibility("hidden")))
 #elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32) || defined(MSC_VER)
 #  define BOOST_EXTENSION_EXPORT_DECL __declspec(dllexport)
 #  define BOOST_EXTENSION_IMPORT_DECL __declspec(dllimport)
-#elif
-#  define BOOST_EXTENSION_EXPORT_DECL
-#  define BOOST_EXTENSION_IMPORT_DECL
 #endif
 
 #endif
