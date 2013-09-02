48102d79c7994713132af07ce35acc8cbf6cd59a
Andrea Michelotti <Andrea.Michelotti@lnf.infn.it>
2013-09-02 16:48:47 +0200

    change the version file just if needed
    Change-Id: I010df82770bd6639597fc0be4b32c9a1662144b3

--- chaos/common/version.sh
+++ chaos/common/version.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 BASEDIR=$(dirname $0)
-echo "Writing version header file" $BASEDIR
+
 
 export PATH=$PATH:/opt/local/bin:/usr/local/bin
 
-git log -n 1 --pretty="format:#define CSLIB_VERSION_HEADER \"!CHAOS Library Developed By Claudio Bisegni\"%n#define CSLIB_VERSION_NUMBER \"Version:%h\"%n#define CSLIB_VERSION_LAST_COMMITTER \"committed by %an\"%n" > $BASEDIR/version.h
+git log -n 1 --pretty="format:#define CSLIB_VERSION_HEADER \"!CHAOS Library Developed By Claudio Bisegni\"%n#define CSLIB_VERSION_NUMBER \"Version:%h\"%n#define CSLIB_VERSION_LAST_COMMITTER \"committed by %an\"%n" > /tmp/version.h 
+if ! diff /tmp/version.h $BASEDIR/version.h; then
+echo "Writing version header file" $BASEDIR
+cp /tmp/version.h $BASEDIR/version.h
+fi
+rm /tmp/version.h
