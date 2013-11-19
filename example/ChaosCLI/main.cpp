10bd0d94d84ef79ae9081d504dca0a0185a59536
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2013-11-19 14:27:18 +0100

    update the CObjectProcessingQueue to use directly the boost thread. Added to the HLDataApi the setting of the global timeout of the DeviceController
    Change-Id: I8a084d4b2ce0da65ca87b788c4943ef3e8a5f12f

--- example/ChaosCLI/main.cpp
+++ example/ChaosCLI/main.cpp
@@ -162,11 +162,10 @@ int main (int argc, char* argv[] )
             }
         }
         
-        DeviceController *controller = HLDataApi::getInstance()->getControllerForDeviceID(deviceID);
+        DeviceController *controller = HLDataApi::getInstance()->getControllerForDeviceID(deviceID, timeout);
         if(!controller) throw CException(4, "Error allcoating decive controller", "device controller creation");
         
         //get the actual state of device
-		controller->setRequestTimeWaith(timeout);
         err = controller->getState(deviceState);
         if(err == ErrorCode::EC_TIMEOUT) throw CException(5, "Time out on connection", "Get state for device");
         
