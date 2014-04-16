8f235ba02fa01fad8cdb5d65831d56d5daae558a
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2014-04-16 17:18:51 +0200

    abstraction of the cacheh algoritm and usage into DeviceSharedDataWorker

---
+++ chaos/common/utility/TemplatedKeyValueHash.h
@@ -0,0 +1,318 @@
+/*
+ *	ConcuredUsedItem.h
+ *	!CHOAS
+ *	Created by Bisegni Claudio.
+ *
+ *    	Copyright 2012 INFN, National Institute of Nuclear Physics
+ *
+ *    	Licensed under the Apache License, Version 2.0 (the "License");
+ *    	you may not use this file except in compliance with the License.
+ *    	You may obtain a copy of the License at
+ *
+ *    	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    	Unless required by applicable law or agreed to in writing, software
+ *    	distributed under the License is distributed on an "AS IS" BASIS,
+ *    	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    	See the License for the specific language governing permissions and
+ *    	limitations under the License.
+ */
+#ifndef __CHAOSFramework__TemplatedHash__
+#define __CHAOSFramework__TemplatedHash__
+
+#include <chaos/common/data/cache/FastHash.h>
+
+#include <boost/thread.hpp>
+
+namespace chaos {
+	namespace common {
+		namespace utility {
+			
+			template<typename T>
+			struct KeyValueHashElement {
+				//! hashed element
+				T element;
+				
+				//! key valueof the lement
+				void *key;
+				
+				//! key length of the value
+				uint32_t key_len;
+				
+				//! Next element having the same hash value
+				KeyValueHashElement *next;
+				
+				//! Previous element having the same hash value
+				KeyValueHashElement *prev;
+			};
+			
+			//! list head for every hash slot
+			template<typename T>
+			struct HashHeadElementList {
+				//!list mutext
+				boost::shared_mutex	mutex_list;
+				
+				//! list point to the head element
+				KeyValueHashElement<T> *head;
+			};
+			
+#define hashsize(n) ((uint32_t)1<<(n))
+#define hashmask(n) (hashsize(n)-1)
+			/*!
+			 Templated fast key value hash container, made on the desing of
+			 the caching model adopted by Memcached. The collision are managed
+			 by a bidirectional list of element having same hash.
+			 */
+			template<typename T, typename H = chaos::common::data::cache::FastHash>
+			class TemplatedKeyValueHash {
+				//symplify the code read
+				
+				//! List head for hash slot
+				typedef HashHeadElementList<T> HashList;
+				
+				//! hashed struct for encapsulate the element
+				typedef KeyValueHashElement<T> HashedStruct;
+				
+				uint32_t		hash_hashpower;
+				uint32_t		hash_mask;
+				HashList		**hash_vector;
+				
+				//! flag for memory the allcoation state of the memory
+				bool			allocation_success;
+				
+				boost::shared_mutex	mutex_hash;
+				
+				//! get the has for the key
+				inline uint32_t getHashForKey(const void * key, const uint32_t key_len) {
+					return H::hash(key, key_len, 0);
+				}
+				
+				//! return the headl of the list identified by the has value
+				/*!
+				 The head is create dinamically when is used for the first time
+				 */
+				inline HashList *getListByHash(uint32_t hash_value) {
+					HashList *_list_head = hash_vector[hash_value & hash_mask];
+					if(!_list_head) {
+						hash_vector[hash_value & hash_mask] = _list_head = new HashList();
+						_list_head->head = NULL;
+					}
+					return _list_head;
+				}
+				
+				//! memory allocation for the vector hash
+				int _allocateHashMemory() {
+					//get real hash vector dimension
+					uint64_t hash_size = hashsize(hash_hashpower);
+					
+					//allcoate hash vector memory
+					hash_vector = (HashList**)calloc(hash_size, sizeof(HashList**));
+					
+					//in case zero all memory
+					if(hash_vector) std::memset(hash_vector, 0, hash_size*sizeof(HashList**));
+					else return -1;
+					
+					//flag success on allcoation
+					allocation_success = true;
+					
+					return 0;
+				}
+				
+				//! relase all hash structures memory
+				void _releaseHashMemory() {
+					if(hash_vector) {
+						for(int idx = 0; idx < hashsize(hash_hashpower); idx++) {
+							if(hash_vector[idx]) delete(hash_vector[idx]);
+						}
+						free(hash_vector);
+					}
+				}
+
+			protected:
+				//! template element deallcoation user method (to override)
+				void clearHashTableElement(const void *key, uint32_t key_len, T element){};
+				
+			public:
+				//! default contructor
+				TemplatedKeyValueHash():
+				hash_hashpower(16),
+				hash_mask(hashmask(hash_hashpower)),
+				hash_vector(NULL),
+				allocation_success(false)  {
+					_allocateHashMemory();
+				}
+				
+				//! parametrized
+				TemplatedKeyValueHash(uint32_t _hash_power):
+				hash_hashpower(_hash_power),
+				hash_mask(hashmask(hash_hashpower)),
+				hash_vector(NULL),
+				allocation_success(false) {
+					_allocateHashMemory();
+				}
+				
+				//! default destructor
+				~TemplatedKeyValueHash() {
+					if(hash_vector) {
+						//clear all element
+						clear();
+						
+						//free hash vector
+						_releaseHashMemory();
+					}
+				}
+				
+				//! check if hashtable memory is valid
+				bool isValid(){
+					return hash_vector!=NULL;
+				}
+				
+				/*!
+				 Get element by his key
+				 */
+				int getElement(const void * key, const uint32_t key_len, T *element_ptr) {
+					int err = -3;
+					//get the list for the has value
+					HashList *_list_head = getListByHash(getHashForKey(key, key_len));
+					
+					// che if the key has an attacched list
+					if(!_list_head) return -1;
+					
+					//lock the list in read
+					boost::shared_lock<boost::shared_mutex> readLock(_list_head->mutex_list);
+					if(!_list_head->head) return -2;
+
+					//appo var for cicling element
+					HashedStruct *_cur_hash_element = _list_head->head;
+					
+					//search need element with key
+					while (_cur_hash_element) {
+						
+						//!compare the length of the key and the memory
+						if ((key_len == _cur_hash_element->key_len) && (memcmp(key, _cur_hash_element->key, key_len) == 0)) {
+							*element_ptr = _cur_hash_element->element;
+							err = 0;
+							break;
+						}
+						_cur_hash_element = _cur_hash_element->next;
+					}
+					return err;
+					
+				}
+				
+				/*!
+				 Add element by key value
+				 */
+				int addElement(const void *key, const uint32_t key_len, T element) {
+					T *element_ptr = NULL;
+					//we can't add two times the same key to hash
+					if(!getElement(key, key_len, element_ptr)) {
+						//another elemento with the same key is preset
+						return -1;
+					}
+					
+					//get the list for the has value
+					HashList *_list_head = getListByHash(getHashForKey(key, key_len));
+					
+					// check if we have got some trouble to allcoate retrieve head list
+					if(!_list_head) return -1;
+					
+					//get the write lock
+					boost::unique_lock<shared_mutex> writeLock(_list_head->mutex_list);
+					
+					//allocate new struct for the element
+					HashedStruct *new_element = new HashedStruct();
+					
+					//check if we have got the memory
+					if(!new_element) return -2;
+					
+					//clear all field of the element
+					std::memset(new_element, 0, sizeof(HashedStruct));
+					
+					//associate the element and key
+					new_element->element = element;
+					new_element->key = malloc(key_len);
+					std::memcpy(new_element->key, key, (new_element->key_len = key_len));
+					
+					//get last slot with same hash
+					HashedStruct *_cur_hash_element = _list_head->head;
+					if(_cur_hash_element) {
+						//incremente the list of collided element
+						_cur_hash_element->prev = new_element;
+						new_element->next = _cur_hash_element;
+					}
+					//add new elemento as first element of collide hash's slot
+					_list_head->head = new_element;
+					
+					//return success
+					return 0;
+				}
+				
+				/*!
+				 Remove the element identified by key
+				 */
+				void removeElement(const void *key, const uint32_t key_len) {
+					//get the list for the has value
+					HashList *_list_head = getListByHash(getHashForKey(key, key_len));
+					
+					//! if the key has no list we return
+					if(!_list_head) return;
+					
+					//get the write lock
+					boost::unique_lock<shared_mutex> writeLock(_list_head->mutex_list);
+					
+					HashedStruct *_cur_hash_element = _list_head->head;
+					while (_cur_hash_element) {
+						if ((key_len == _cur_hash_element->key_len) && (memcmp(key, _cur_hash_element->key, key_len) == 0)) {
+							if(_cur_hash_element->prev) {
+								_cur_hash_element->next->prev = _cur_hash_element->prev;
+							} else {
+								//is the first of the list so set the next as first
+								_list_head->head = _cur_hash_element->next;
+							}
+							break;
+						} else {
+							//go ahead
+							_cur_hash_element = _cur_hash_element->next;
+						}
+					}
+				}
+				
+				//!clear all hash table
+				void clear() {
+					
+					//clear all element(not the hash vector)
+					for(int idx = 0; idx < hashsize(hash_hashpower); idx++) {
+						
+						//chec if the current slot as an attacche list
+						if(!hash_vector[idx]) continue;
+						
+						HashedStruct *to_delete_slot = NULL;
+						HashedStruct *cur_slot = hash_vector[idx]->head;
+						
+						//achieve a write lock for access list
+						boost::unique_lock<boost::shared_mutex> readLock(hash_vector[idx]->mutex_list);
+						
+						while(cur_slot) {
+							//get reference to slot that need to be deleted
+							to_delete_slot = cur_slot;
+							
+							//go ahead in list
+							cur_slot = cur_slot->next;
+							
+							//call user overloaded (possible) clear element
+							clearHashTableElement(to_delete_slot->key, to_delete_slot->key_len, to_delete_slot->element);
+							
+							//delete slot
+							delete(to_delete_slot);
+							
+							//go ahead
+						}
+					}
+				}
+			};
+		}
+	}
+}
+
+#endif /* defined(__CHAOSFramework__TemplatedHash__) */
