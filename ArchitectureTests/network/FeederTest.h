e0bd298309fd39553b5f0a7036fe777d76b7b7f5
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2014-06-17 10:17:04 +0200

    restructured tests

---
+++ ArchitectureTests/network/FeederTest.h
@@ -0,0 +1,43 @@
+//
+//  FeederTest.h
+//  CHAOSFramework
+//
+//  Created by Claudio Bisegni on 28/05/14.
+//  Copyright (c) 2014 INFN. All rights reserved.
+//
+
+#ifndef __CHAOSFramework__FeederTest__
+#define __CHAOSFramework__FeederTest__
+
+#include <chaos/common/network/URLServiceFeeder.h>
+
+namespace chaos {
+    namespace test {
+        namespace network {
+            
+            class ServiceForURL {
+            public:
+                std::string url;
+            };
+            
+            class FeederTest : public common::network::URLServiceFeederHandler {
+				chaos::common::network::URLServiceFeeder feeder_engine;
+				uint32_t number_of_cycle;
+            protected:
+                
+                void disposeService(void *service_ptr);
+                void* serviceForURL(common::network::URL url);
+				
+				void test_feeder_logic();
+				void test_feeder_logic_equal_priority();
+				void test_feeder_preformance();
+			public:
+				void test(uint32_t _number_of_cycle = 0);
+				FeederTest();
+            };
+            
+        }
+    }
+}
+
+#endif /* defined(__CHAOSFramework__FeederTest__) */
