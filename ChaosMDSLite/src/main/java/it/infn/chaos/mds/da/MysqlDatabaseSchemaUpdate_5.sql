c5a2b0b6aa37af53e342b74e747b5822d430f171
Claudio <Claudio.Bisegni@lnf.infn.it>
2015-07-17 10:28:33 +0200

    removed old metadata server lite edition, now is replaced by ChaosMetadataService

--- ChaosMDSLite/src/main/java/it/infn/chaos/mds/da/MysqlDatabaseSchemaUpdate_5.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `chaosms`.`unit_server` 
-ADD COLUMN `private_key` VARCHAR(1024) NULL AFTER `unit_server_hb_time`,
-ADD COLUMN `public_key` VARCHAR(1024) NULL AFTER `private_key`,
-ADD COLUMN `reg_state` INT NULL AFTER `public_key`;
--- ChaosMDSLite/src/main/java/it/infn/chaos/mds/da/MysqlDatabaseSchemaUpdate_8.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `chaosms`.`unit_server_cu_instance` CHANGE COLUMN `cu_param` `cu_param` MEDIUMTEXT NULL DEFAULT NULL ;
