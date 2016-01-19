36b0bdd5d88a47f7b20525d291506b28224c7938
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2016-01-19 15:39:14 +0100

    fix of throw of bad exception

--- chaos/cu_toolkit/control_manager/slow_command/SlowCommandExecutor.cpp
+++ chaos/cu_toolkit/control_manager/slow_command/SlowCommandExecutor.cpp
@@ -44,8 +44,8 @@ using namespace chaos::common::data::cache;
 using namespace chaos::common::batch_command;
 
 SlowCommandExecutor::SlowCommandExecutor(const std::string& _executorID,
-										 DatasetDB *_dataset_attribute_db_ptr,
-										 SCAbstractControlUnit *_control_unit_instance):
+                                         DatasetDB *_dataset_attribute_db_ptr,
+                                         SCAbstractControlUnit *_control_unit_instance):
 BatchCommandExecutor(_executorID),
 dataset_attribute_db_ptr(_dataset_attribute_db_ptr),
 attribute_cache(new AttributeSharedCacheWrapper(getAttributeSharedCache())),
@@ -57,64 +57,64 @@ last_error_domain(NULL),
 driverAccessorsErogator(NULL){}
 
 SlowCommandExecutor::~SlowCommandExecutor(){
-	//delete the wrapper
-	if(attribute_cache) {
-		delete(attribute_cache);
-	}
+    //delete the wrapper
+    if(attribute_cache) {
+        delete(attribute_cache);
+    }
 }
 
 // Initialize instance
 void SlowCommandExecutor::init(void *initData) throw(chaos::CException) {
-	//initialize superclass
-	BatchCommandExecutor::init(initData);
+    //initialize superclass
+    BatchCommandExecutor::init(initData);
 }
 
 // Start the implementation
 void SlowCommandExecutor::start() throw(chaos::CException) {
-	//the default command, if there is is launched here so we need to update the system dataaset
-	const std::string& command_alias = getDefaultCommand();
-	if(command_alias.size()) {
-		AttributeValue *attr_value = getAttributeSharedCache()->getAttributeValue(DOMAIN_INPUT, command_alias);
-		if(attr_value) {
-			std::string cmd_param = "none";
-			//add new size
-			attr_value->setNewSize((uint32_t)cmd_param.size());
-
-			//set the value without notify because command value are managed internally only
-			attr_value->setValue(cmd_param.c_str(), (uint32_t)cmd_param.size(), false);
-
-			//push input dataset change
-			control_unit_instance->pushInputDataset();
-		}
-	}
-	//start superclass
-	BatchCommandExecutor::start();
+    //the default command, if there is is launched here so we need to update the system dataaset
+    const std::string& command_alias = getDefaultCommand();
+    if(command_alias.size()) {
+        AttributeValue *attr_value = getAttributeSharedCache()->getAttributeValue(DOMAIN_INPUT, command_alias);
+        if(attr_value) {
+            std::string cmd_param = "none";
+            //add new size
+            attr_value->setNewSize((uint32_t)cmd_param.size());
+            
+            //set the value without notify because command value are managed internally only
+            attr_value->setValue(cmd_param.c_str(), (uint32_t)cmd_param.size(), false);
+            
+            //push input dataset change
+            control_unit_instance->pushInputDataset();
+        }
+    }
+    //start superclass
+    BatchCommandExecutor::start();
 }
 
 
 // Start the implementation
 void SlowCommandExecutor::stop() throw(chaos::CException) {
-	LDBG_ << "No implementation on stop";
-	//initialize superclass
-	BatchCommandExecutor::stop();
+    LDBG_ << "No implementation on stop";
+    //initialize superclass
+    BatchCommandExecutor::stop();
 }
 
 // Deinit instance
 void SlowCommandExecutor::deinit() throw(chaos::CException) {
-	LDBG_ << "No implementation on deinit";
-	//initialize superclass
-	BatchCommandExecutor::deinit();
-	last_ru_id_cache = NULL;
-	last_error_code = NULL;
-	last_error_message = NULL;
-	last_error_domain = NULL;
+    LDBG_ << "No implementation on deinit";
+    //initialize superclass
+    BatchCommandExecutor::deinit();
+    last_ru_id_cache = NULL;
+    last_error_code = NULL;
+    last_error_message = NULL;
+    last_error_domain = NULL;
 }
 
 //! Install a command associated with a type
 void SlowCommandExecutor::installCommand(const string& alias,
                                          chaos::common::utility::NestedObjectInstancer<SlowCommand, chaos_batch::BatchCommand> *instancer) {
-	//call superclss method
-	BatchCommandExecutor::installCommand(alias, instancer);
+    //call superclss method
+    BatchCommandExecutor::installCommand(alias, instancer);
 }
 
 //! Install a command
@@ -126,105 +126,95 @@ void SlowCommandExecutor::installCommand(boost::shared_ptr<BatchCommandDescripti
 //! Check if the waithing command can be installed
 BatchCommand *SlowCommandExecutor::instanceCommandInfo(const std::string& command_alias,
                                                       	uint32_t submission_rule,
-                                                        uint32_t submission_retry_delay,
-                                                        uint64_t scheduler_step_delay) {
-	//install command into the batch command executor root class
-	SlowCommand *result = (SlowCommand*) BatchCommandExecutor::instanceCommandInfo(command_alias,
-                                                                                 submission_rule,
-																																								 submission_retry_delay,
-																																								 scheduler_step_delay);
-
-	//cusotmize slow command sublcass
-	if(result) {
-		//forward the pointers of the needed data
-		result->driverAccessorsErogator = driverAccessorsErogator;
-		result->dataset_attribute_db_ptr = dataset_attribute_db_ptr;
-		result->attribute_cache = attribute_cache;
-	}
-	return result;
+                                                       uint32_t submission_retry_delay,
+                                                       uint64_t scheduler_step_delay) {
+    //install command into the batch command executor root class
+    SlowCommand *result = (SlowCommand*) BatchCommandExecutor::instanceCommandInfo(command_alias,
+                                                                                   submission_rule,
+                                                                                   submission_retry_delay,
+                                                                                   scheduler_step_delay);
+    
+    //cusotmize slow command sublcass
+    if(result) {
+        //forward the pointers of the needed data
+        result->driverAccessorsErogator = driverAccessorsErogator;
+        result->dataset_attribute_db_ptr = dataset_attribute_db_ptr;
+        result->attribute_cache = attribute_cache;
+    }
+    return result;
 }
 //overlodaed command event handler
 void SlowCommandExecutor::handleCommandEvent(uint64_t command_seq,
-											 BatchCommandEventType::BatchCommandEventType type,
-											 void* type_value_ptr,
-											 uint32_t type_value_size) {
-	//let the base class handle the event
-	BatchCommandExecutor::handleCommandEvent(command_seq,
-											 type,
-											 type_value_ptr,
-											 type_value_size);
-
-	switch(type) {
-		case BatchCommandEventType::EVT_QUEUED: {
-			break;
-		}
-		case BatchCommandEventType::EVT_FAULT: {
-			if(type_value_size == sizeof(FaultDescription)) {
-				FaultDescription *faul_desc = static_cast<FaultDescription*>(type_value_ptr);
-				/*if(!last_error_code) {
-					last_error_code = getAttributeSharedCache()->getAttributeValue(DOMAIN_SYSTEM,
-																				   DataPackSystemKey::DP_SYS_LAST_ERROR);
-				}
-				if(!last_error_message) {
-					last_error_message = getAttributeSharedCache()->getAttributeValue(DOMAIN_SYSTEM,
-																					  DataPackSystemKey::DP_SYS_LAST_ERROR_MESSAGE);
-				}
-				if(!last_error_domain) {
-					last_error_domain = getAttributeSharedCache()->getAttributeValue(DOMAIN_SYSTEM,
-																					 DataPackSystemKey::DP_SYS_LAST_ERROR_DOMAIN);
-				}
-				//write error on cache
-				//last_error_code->setValue(&faul_desc->code, sizeof(int32_t));
-				//last_error_message->setValue(faul_desc->description.c_str(), (int32_t)faul_desc->description.size());
-				//last_error_domain->setValue(faul_desc->domain.c_str(), (int32_t)faul_desc->domain.size());*/
-                CException ex(faul_desc->code, faul_desc->description, faul_desc->domain);
-                //async go into recoverable error
-                boost::thread(boost::bind(&AbstractControlUnit::_goInRecoverableError, control_unit_instance, ex)).detach();
-
-			}
-			break;
-		}
-		default:
-			break;
-	}
+                                             BatchCommandEventType::BatchCommandEventType type,
+                                             void* type_value_ptr,
+                                             uint32_t type_value_size) {
+    //let the base class handle the event
+    BatchCommandExecutor::handleCommandEvent(command_seq,
+                                             type,
+                                             type_value_ptr,
+                                             type_value_size);
+    
+    switch(type) {
+        case BatchCommandEventType::EVT_QUEUED: {
+            break;
+        }
+        case BatchCommandEventType::EVT_FAULT: {
+            if(type_value_size == sizeof(FaultDescription)) {
+                FaultDescription *faul_desc = static_cast<FaultDescription*>(type_value_ptr);
+                //TODO doing it in better solution
+                if(faul_desc->code &&
+                   faul_desc->description.size() &&
+                   faul_desc->domain) {
+                    CException ex(faul_desc->code, faul_desc->description, faul_desc->domain);
+                    //async go into recoverable error
+                    boost::thread(boost::bind(&AbstractControlUnit::_goInRecoverableError, control_unit_instance, ex)).detach();
+                } else {
+                    SCELERR_ << "COmmand id " << command_seq << " gone in fault without exception";
+                }
+            }
+            break;
+        }
+        default:
+            break;
+    }
 }
 
 //! general sandbox event handler
 void SlowCommandExecutor::handleSandboxEvent(const std::string& sandbox_id,
-											 BatchSandboxEventType::BatchSandboxEventType type,
-											 void* type_value_ptr,
-											 uint32_t type_value_size) {
-	//let the base class handle the event
-	BatchCommandExecutor::handleSandboxEvent(sandbox_id, type, type_value_ptr, type_value_size);
-
-	if(!last_ru_id_cache) {
-		last_ru_id_cache = getAttributeSharedCache()->getAttributeValue(DOMAIN_SYSTEM,
-																		DataPackSystemKey::DP_SYS_RUN_UNIT_ID);
-		if(!last_ru_id_cache) {
-			SCELERR_ << "Error getting cache slot for unit id";
-			return;
-		}
-	}
-
-	//set the last system event sandbox id
-	last_ru_id_cache->setValue(sandbox_id.c_str(), (uint32_t)sandbox_id.size());
-
-	switch(type) {
-		case BatchSandboxEventType::EVT_RUN_START: {
-			uint64_t *hb = static_cast<uint64_t*>(type_value_ptr);
+                                             BatchSandboxEventType::BatchSandboxEventType type,
+                                             void* type_value_ptr,
+                                             uint32_t type_value_size) {
+    //let the base class handle the event
+    BatchCommandExecutor::handleSandboxEvent(sandbox_id, type, type_value_ptr, type_value_size);
+    
+    if(!last_ru_id_cache) {
+        last_ru_id_cache = getAttributeSharedCache()->getAttributeValue(DOMAIN_SYSTEM,
+                                                                        DataPackSystemKey::DP_SYS_RUN_UNIT_ID);
+        if(!last_ru_id_cache) {
+            SCELERR_ << "Error getting cache slot for unit id";
+            return;
+        }
+    }
+    
+    //set the last system event sandbox id
+    last_ru_id_cache->setValue(sandbox_id.c_str(), (uint32_t)sandbox_id.size());
+    
+    switch(type) {
+        case BatchSandboxEventType::EVT_RUN_START: {
+            uint64_t *hb = static_cast<uint64_t*>(type_value_ptr);
             control_unit_instance->_updateAcquistionTimestamp(*hb);
-
-		}
-
+            
+        }
+            
         case BatchSandboxEventType::EVT_RUN_END: {
             //push output dataset specifingthat the ts has been already updated in casche directly
             control_unit_instance->pushOutputDataset(true);
             break;
         }
-
+            
         case BatchSandboxEventType::EVT_UPDATE_RUN_DELAY:
             control_unit_instance->_updateRunScheduleDelay(*static_cast<uint64_t*>(type_value_ptr));
             control_unit_instance->pushSystemDataset();
             break;
-	}
+    }
 }
