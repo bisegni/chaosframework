746853ac4935a65702bdc2f6be85a929dddc7fc1
Claudio <Claudio.Bisegni@lnf.infn.it>
2016-02-10 15:49:09 +0100

    fix unwanted crash

--- ChaosWANProxy/api/producer/ProducerInsertDatasetApi.cpp
+++ ChaosWANProxy/api/producer/ProducerInsertDatasetApi.cpp
@@ -120,7 +120,7 @@ int ProducerInsertDatasetApi::execute(std::vector<std::string>& api_tokens,
 			PRODUCER_INSERT_ERR(output_data, -4, err_msg);
 			return err;
 		} else if(!dataset_element.isString()) {
-		  err_msg = "The dataset element needs to be only string:"+dataset_element.asString();
+            err_msg = "The dataset element needs to be only string";
 			PID_LERR << err_msg;
 			PRODUCER_INSERT_ERR(output_data, -5, err_msg);
 			return err;
