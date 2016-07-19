8c7a54689343b4d139638ec2746f66bc9ebaf5a6
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2016-07-19 13:55:54 +0200

    added api to user helper class for create catalog

--- chaos/common/status_manager/DeclareStatusFlag.cpp
+++ chaos/common/status_manager/DeclareStatusFlag.cpp
@@ -39,10 +39,30 @@ void DeclareStatusFlag::addBoolFlag(const std::string flag_name,
     addFlag(bool_flag);
 }
 
-void DeclareStatusFlag::addLevelFlag(const std::string flag_name,
-                                     const std::string flag_description,
-                                     const StateLevelContainer& map_code_level_state) {
+void DeclareStatusFlag::addLevelFlagWithStates(const std::string flag_name,
+                                               const std::string flag_description,
+                                               const StateLevelContainer& map_code_level_state) {
     boost::shared_ptr<StatusFlag> level_flag(new StatusFlag(flag_name, flag_description));
     level_flag->addLevelsFromSet(map_code_level_state);
     addFlag(level_flag);
+}
+
+void DeclareStatusFlag::addFlag(const std::string flag_name,
+                                const std::string flag_description) {
+    addFlag(boost::shared_ptr<StatusFlag>(new StatusFlag(flag_name,
+                                                         flag_description)));
+}
+
+void DeclareStatusFlag::addStateToFlag(const std::string flag_name,
+                                       const StateLevel& new_state) {
+    boost::shared_ptr<StatusFlag> flag = getFlagByName(flag_name);
+    if(flag.get() == NULL) return;
+    flag->addLevel(new_state);
+}
+
+void DeclareStatusFlag::setFlagState(const std::string flag_name,
+                                     const int8_t set_state) {
+    boost::shared_ptr<StatusFlag> flag = getFlagByName(flag_name);
+    if(flag.get() == NULL) return;
+    flag->setCurrentLevel(set_state);
 }
