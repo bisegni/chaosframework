5637f4184b5297755e07b7faf2445ef643e9a0b4
Andrea Michelotti <Andrea.Michelotti@lnf.infn.it>
2016-08-03 16:33:35 +0200

    patched for clang

--- tools/common_util.sh
+++ tools/common_util.sh
@@ -1,11 +1,11 @@
 
 OS=`uname -s`
 ARCH=`uname -m`
-realpath=`readlink -f $0`
-SCRIPTNAME=`basename $realpath`
+
 SCRIPTTESTPATH=$0
 pushd `dirname $0` > /dev/null
 SCRIPTTESTABSPATH=`pwd -P`
+SCRIPTNAME=`basename $SCRIPTTESTABSPATH`
 popd > /dev/null
 if [ -n "$CHAOS_TOOLS" ];then
     tools=$CHAOS_TOOLS
