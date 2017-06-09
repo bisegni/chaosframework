ce919c71819ee76f2c234af59626d3d28558a318
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2017-06-09 19:48:57 +0200

    bring zmq client timeout to same value as high level api do 5 seconds. Timeout of high level and lower need to be managed with same value

--- chaos/common/rpc/zmq/ZMQClient.cpp
+++ chaos/common/rpc/zmq/ZMQClient.cpp
@@ -51,7 +51,7 @@ static void my_free (void *data, void *hint) {
 ZMQClient::ZMQClient(const string& alias):
 RpcClient(alias),
 zmq_context(NULL),
-zmq_timeout(1000){}
+zmq_timeout(5000){}
 
 ZMQClient::~ZMQClient(){}
 
