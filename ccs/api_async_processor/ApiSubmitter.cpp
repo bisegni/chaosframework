4c03a95a310695e58ba7af909f0cf3137c88a7b4
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2017-06-12 17:49:11 +0200

    fix a little bug that occours when an rpc message has been submitted and remote hosti is shoutdown so i receive the NULL value in  api 'apiDone'

--- ccs/api_async_processor/ApiSubmitter.cpp
+++ ccs/api_async_processor/ApiSubmitter.cpp
@@ -6,35 +6,36 @@ using namespace chaos::metadata_service_client::api_proxy;
 
 ApiSubmitter::ApiSubmitter(ApiHandler *_api_handler, QObject *parent):
     QObject(parent),
-api_handler(_api_handler){}
+    api_handler(_api_handler){}
 
 //!submit api result for async wait termination
 void ApiSubmitter::submitApiResult(const QString& api_tag,
-                     chaos::metadata_service_client::api_proxy::ApiProxyResult api_result) {
+                                   chaos::metadata_service_client::api_proxy::ApiProxyResult api_result) {
     CHAOS_ASSERT(api_handler)
-    api_processor.submitApiResult(api_tag,
-                                  std::move(api_result),
-                                  (QObject*)this,
-                                  SLOT(asyncApiResult(QString, QSharedPointer<chaos::common::data::CDataWrapper>)),
-                                  SLOT(asyncApiError(QString, QSharedPointer<chaos::CException>)),
-                                  SLOT(asyncApiTimeout(QString)));
+            api_processor.submitApiResult(api_tag,
+                                          std::move(api_result),
+                                          (QObject*)this,
+                                          SLOT(asyncApiResult(QString, QSharedPointer<chaos::common::data::CDataWrapper>)),
+                                          SLOT(asyncApiError(QString, QSharedPointer<chaos::CException>)),
+                                          SLOT(asyncApiTimeout(QString)));
     api_handler->apiHasStarted(api_tag);
 }
 
 void ApiSubmitter::asyncApiResult(const QString& tag,
-                                     QSharedPointer<chaos::common::data::CDataWrapper> api_result) {
+                                  QSharedPointer<chaos::common::data::CDataWrapper> api_result) {
     CHAOS_ASSERT(api_handler)
+    if(api_result == NULL) return;
     api_handler->onApiDone(tag,
-              api_result);
+                           api_result);
     //emit signal that we are finisched the wait on api result
     api_handler->apiHasEnded(tag);
 }
 
 void ApiSubmitter::asyncApiError(const QString& tag,
-                                    QSharedPointer<chaos::CException> api_exception) {
+                                 QSharedPointer<chaos::CException> api_exception) {
     CHAOS_ASSERT(api_handler)
-    api_handler->onApiError(tag,
-               api_exception);
+            api_handler->onApiError(tag,
+                                    api_exception);
     api_handler->apiHasEndedWithError(tag,
                                       api_exception);
     api_handler->apiHasEnded(tag);
@@ -42,7 +43,7 @@ void ApiSubmitter::asyncApiError(const QString& tag,
 
 void ApiSubmitter::asyncApiTimeout(const QString& tag) {
     CHAOS_ASSERT(api_handler)
-    api_handler->onApiTimeout(tag);
+            api_handler->onApiTimeout(tag);
     api_handler->apiHasEndedWithError(tag,
                                       QSharedPointer<chaos::CException>(new chaos::CException(-1, "ApiTimeout", "ApiSubmitter::asyncApiTimeout")));
     api_handler->apiHasEnded(tag);
