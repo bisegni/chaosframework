879a0d573a44356463687ee2563d6f9894007500
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2017-08-07 14:27:02 +0200

    fix

--- chaos_micro_unit_toolkit/micro_unit_toolkit_types.h
+++ chaos_micro_unit_toolkit/micro_unit_toolkit_types.h
@@ -8,25 +8,26 @@
 
 #ifndef micro_unit_toolkit_types_h
 #define micro_unit_toolkit_types_h
-
 #include <memory>
-
 namespace chaos {
     namespace micro_unit_toolkit {
 #if __cplusplus >= 201103L
-        template<typename T>
-        using ChaosSharedPtr = std::shared_ptr<T>;
-        template<typename T>
-        using ChaosUniquePtr = std::unique_ptr<T>;
+template<typename T>
+using ChaosUniquePtr = std::unique_ptr<T>;
+  #ifndef FORCE_BOOST_SHPOINTER
+    template<typename T>
+    using ChaosSharedPtr = std::shared_ptr<T>;
+  #else
+    #include <boost/shared_ptr.hpp>
+    #define ChaosSharedPtr boost::shared_ptr
+  #endif
 #define ChaosMoveOperator(x) std::move(x)
 #else
 #include <boost/shared_ptr.hpp>
 #define ChaosSharedPtr boost::shared_ptr
-#define ChaosUniquePtr std::auto_ptr;
+#define ChaosUniquePtr std::auto_ptr
 #define ChaosMoveOperator(x) x
-
 #endif
-
         static unsigned int CommunicationTimeout = 5000;
     }
 }
