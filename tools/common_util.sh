49636be93e8b0f0c672e38b10a02c12c80b37cfd
amichelo <andrea.michelotti@lnf.infn.it>
2017-09-08 15:58:15 +0200

    copy test too

--- tools/common_util.sh
+++ tools/common_util.sh
@@ -919,7 +919,7 @@ launch_us_cu(){
 	    local curr_reg=0
 	    var1="((\`grep \"successfully registered\" $CHAOS_PREFIX/log/$USNAME-$FILE_NAME.log |wc -l\` >= $NCU))"
 	    while [ $curr_reg -gt $old_reg ] && [ $curr_reg -lt $NCU ] ;do
-		execute_command_until_ok "$var1" 10
+		execute_command_until_ok "$var1" 20
 		old_reg=$curr_reg
 		curr_reg=`grep "successfully registered" $CHAOS_PREFIX/log/$USNAME-$FILE_NAME.log |wc -l`
 		echo ""
