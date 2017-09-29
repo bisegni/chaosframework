41baf06903d52f79cbfeb8b6e73399bf23f5cef3
amichelo <andrea.michelotti@lnf.infn.it>
2017-09-30 00:31:26 +0200

    better format

--- chaos/common/version.sh
+++ chaos/common/version.sh
@@ -12,7 +12,7 @@ fi
 
 #check if we are using the script into xcode or no (in xcode the ssty give an error)
 if [ ! -n $XCODE_PRODUCT_BUILD_VERSION ]; then
-    git log -n 1 --pretty="format:#define CSLIB_VERSION_HEADER \"!CHAOS Library Developed By Claudio Bisegni\"%n#define CSLIB_VERSION_NUMBER \"Version:%h\"%n" > /tmp/version.h
+    git log -n 1 --pretty="format:#define CSLIB_VERSION_HEADER \"!CHAOS Library Developed By Claudio Bisegni\"%n#define CSLIB_VERSION_NUMBER \"%h\"%n" > /tmp/version.h
     stty -echo
 
     if ! diff /tmp/version.h $BASEDIR/version.h>/dev/null; then
@@ -23,5 +23,5 @@ if [ ! -n $XCODE_PRODUCT_BUILD_VERSION ]; then
     rm /tmp/version.h
     stty echo
 else
-    git log -n 1 --pretty="format:#define CSLIB_VERSION_HEADER \"!CHAOS Library Developed By Claudio Bisegni\"%n#define CSLIB_VERSION_NUMBER \"Version:%h\"%n" > $BASEDIR/version.h
+    git log -n 1 --pretty="format:#define CSLIB_VERSION_HEADER \"!CHAOS Library Developed By Claudio Bisegni\"%n#define CSLIB_VERSION_NUMBER \"%h\"%n" > $BASEDIR/version.h
 fi
