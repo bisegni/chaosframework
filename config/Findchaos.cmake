791bef50ee2936c3066ee7f4ba302c6279a1c3db
amichelo <andrea.michelotti@lnf.infn.it>
2018-05-31 17:21:07 +0200

    forgot

---
+++ config/Findchaos.cmake
@@ -0,0 +1,6 @@
+set(PREFIX ${CMAKE_CURRENT_LIST_DIR})
+set(chaos_INCLUDE_DIRS ${PREFIX}/include)
+FILE(GLOB boost_libs ${PREFIX}/lib/libboost*.a)
+set(chaos_LIBRARIES ${PREFIX}/lib/libchaos_metadata_service_client.so ${PREFIX}/lib/libchaos_common.so ${PREFIX}/lib/libchaos_cutoolkit.so pthread dl)
+SET(CMAKE_INSTALL_RPATH "${PREFIX}/lib")
+link_directories(${PREFIX}/lib)
