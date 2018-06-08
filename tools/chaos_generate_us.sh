8c34b86f8ae3e585327bc263b332cceffd35d560
amichelo <andrea.michelotti@lnf.infn.it>
2018-06-08 22:41:01 +0200

    unit server generator

--- tools/chaos_generate_us.sh
+++ tools/chaos_generate_us.sh
@@ -314,6 +314,9 @@ echo -e "\t\tchaos::cu::ChaosCUToolkit::getInstance()->start();" >> $project_dir
 echo -e "\t} catch (CException& e) {\n\t\tstd::cerr<<\"Exception:\"<<std::endl;\n\t\tstd::cerr<< \"domain	:\"<<e.errorDomain << std::endl;\n\t\tstd::cerr<< \"cause	:\"<<e.errorMessage << std::endl;return -1;\n\t} catch (program_options::error &e){\n\t\tstd::cerr << "\"Unable to parse command line: \"" << e.what() << std::endl;return -2;\n\t} catch (...){\n\t\tstd::cerr << \"unexpected exception caught.. \" << std::endl;return -3;\n\t}return 0;\n}\n" >> $project_dir/main.cpp
 
 echo "cmake_minimum_required(VERSION 2.8)" > $project_dir/CMakeLists.txt
+echo "find_package(chaos MODULE REQUIRED)" >> $project_dir/CMakeLists.txt
+echo "include_directories(../../ \${chaos_INCLUDE_DIRS})">> $project_dir/CMakeLists.txt
+
 echo "SET(src main.cpp )" >>  $project_dir/CMakeLists.txt
 echo -e "$cmake_things" >> $project_dir/CMakeLists.txt
 if [ -n "$incdir_list" ]; then
