769b6b13ef28b07290863697fe38a70c37b0af64
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2018-07-12 10:47:18 +0200

    add rpc test and fixed shutdown of chaos/common/direct_io/impl/ZMQDirectIOServer.cpp class

--- chaos/common/direct_io/DirectIODispatcher.h
+++ chaos/common/direct_io/DirectIODispatcher.h
@@ -21,6 +21,7 @@
 #ifndef __CHAOSFramework__AbstractDirectIODispatcher__
 #define __CHAOSFramework__AbstractDirectIODispatcher__
 
+#include <chaos/common/chaos_types.h>
 #include <chaos/common/direct_io/DirectIOHandler.h>
 #include <chaos/common/direct_io/DirectIOServerEndpoint.h>
 #include <chaos/common/utility/StartableService.h>
@@ -33,10 +34,7 @@ namespace chaos {
 	namespace common {
 		namespace direct_io {
             class DirectIOServer;
-			//boost::function2<void, void*, uint32_t> delegate = priority_service?
-           // boost::bind(&DirectIOHandler::serviceDataReceived, handler_impl, _1, _2):
-           // boost::bind(&DirectIOHandler::priorityDataReceived, handler_impl, _1, _2);
-			//! Default dispatcher for the direct io system
+
 			class DirectIODispatcher:
 			public common::direct_io::DirectIOHandler,
 			public utility::StartableService {
@@ -46,9 +44,9 @@ namespace chaos {
 					bool enable;
 					DirectIOServerEndpoint *endpoint;
 				};
-
-				
-				//! available endpoint slotc
+                
+				//! available endpoint slot
+                ChaosSharedMutex slot_mutex;
 				EndpointFastDelegation * * endpoint_slot_array;
 				
 				//!available index queue
