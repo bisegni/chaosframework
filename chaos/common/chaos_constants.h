3728a7463ef23ccc43de2dbab290c2e11d3fa310
amichelo <andrea.michelotti@lnf.infn.it>
2018-09-28 13:49:14 +0200

    initialized seq_id

--- chaos/common/chaos_constants.h
+++ chaos/common/chaos_constants.h
@@ -172,7 +172,7 @@ namespace chaos {
         	static const unsigned int AgentTimersTimeoutinMSec                  = 5000;
         	static const unsigned int CUTimersTimeoutinMSec                     = 5000;
         	static const unsigned int PerformanceManagerTimersTimeoutinMSec     = 5000;
-            static const unsigned int ObjectStorageTimeoutinMSec                = 5000;
+            static const unsigned int ObjectStorageTimeoutinMSec                = 15000;
             //!time to wait for queue can accept new data to push in object storage
             /*!
              Mds when receive a new dataset to store on history, it is push on hst sublayer
@@ -180,7 +180,7 @@ namespace chaos {
              mds to permit queue to can accept this new dataset. after that the new dataset
              is nto accepted by sublayer.
              */
-            static const unsigned int MDSHistoryQueuePushTimeoutinMSec          = 5000;
+            static const unsigned int MDSHistoryQueuePushTimeoutinMSec          = 15000;
     	}
 
     };
