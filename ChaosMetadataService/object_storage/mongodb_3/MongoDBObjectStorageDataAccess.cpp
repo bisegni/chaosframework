7b13ca8f3217de9e503bc2da979347b13aa07319
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2019-04-30 09:27:25 +0200

    fix query

--- ChaosMetadataService/object_storage/mongodb_3/MongoDBObjectStorageDataAccess.cpp
+++ ChaosMetadataService/object_storage/mongodb_3/MongoDBObjectStorageDataAccess.cpp
@@ -305,13 +305,17 @@ int MongoDBObjectStorageDataAccess::findObject(const std::string&               
         if(reverse_order == false) {
             time_builder.append(kvp("$gte", b_date(std::chrono::milliseconds(timestamp_from))));
             time_builder.append(kvp("$lte", b_date(std::chrono::milliseconds(timestamp_to))));
+            builder.append(kvp(std::string(chaos::DataPackCommonKey::DPCK_TIMESTAMP), time_builder.view()));
+            builder.append(kvp(run_key,     make_document(kvp("$gte", last_record_found_seq.run_id))));
+            builder.append(kvp(counter_key, make_document(kvp("$gte",  last_record_found_seq.datapack_counter ))));
         }else{
             time_builder.append(kvp("$lte", b_date(std::chrono::milliseconds(timestamp_from))));
             time_builder.append(kvp("$gte", b_date(std::chrono::milliseconds(timestamp_to))));
+            builder.append(kvp(std::string(chaos::DataPackCommonKey::DPCK_TIMESTAMP), time_builder.view()));
+            builder.append(kvp(run_key,     make_document(kvp("$lte", last_record_found_seq.run_id))));
+            builder.append(kvp(counter_key, make_document(kvp("$lte",  last_record_found_seq.datapack_counter ))));
         }
-        builder.append(kvp(std::string(chaos::DataPackCommonKey::DPCK_TIMESTAMP), time_builder.view()));
-        builder.append(kvp(run_key,     make_document(kvp("$lte", last_record_found_seq.run_id))));
-        builder.append(kvp(counter_key, make_document(kvp("$lte",  last_record_found_seq.datapack_counter ))));
+
         if(meta_tags.size()) {
             auto array_builder = bsoncxx::builder::basic::array{};
             for(auto& it: meta_tags) {
